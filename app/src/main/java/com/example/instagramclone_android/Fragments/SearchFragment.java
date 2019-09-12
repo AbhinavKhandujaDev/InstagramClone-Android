e8978f7dbbc5a2d1220264d9c71f16f373db6f4f
Abhinav <abhikhanduja@gmail.com>
2019-09-12 23:13:33 +0530

    Initially only one fragment in bottom navigation will be loaded, others will be loaded when item tapped and will remain in memory to reduce the networking calls

--- app/src/main/java/com/example/instagramclone_android/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/instagramclone_android/Fragments/SearchFragment.java
@@ -1,6 +1,7 @@
 package com.example.instagramclone_android.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,11 @@ import androidx.fragment.app.Fragment;
 import com.example.instagramclone_android.R;
 
 public class SearchFragment extends Fragment {
+    private static final String TAG = "SearchFragment";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "frag onCreateView");
         return inflater.inflate(R.layout.fragment_search, container,false);
     }
 }
--- app/src/main/java/com/example/instagramclone_android/MainActivity.java
+++ app/src/main/java/com/example/instagramclone_android/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.instagramclone_android.Fragments.HomeFragment;
 import com.example.instagramclone_android.Fragments.NotificationsFragment;
@@ -22,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+    private Fragment homeFrag;
+    private Fragment searchFrag;
+    private Fragment notifFrag;
+    private Fragment profileFrag;
+
+    private Fragment currentFrag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,38 +43,74 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        toolbarTitle.setText("Home");
-        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, new HomeFragment()).commit();
-
+        setRootFragment();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFrag = null;
 
+            if (menuItem.getItemId() == bottomNavigationView.getSelectedItemId()) { return false; }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            transaction.hide(currentFrag);
+
+            //At first only one fragment is loaded to reduce the load of networking calls
             switch (menuItem.getItemId()) {
                 case R.id.nav_home:
                     toolbarTitle.setText("Home");
-                    selectedFrag = new HomeFragment();
+                    currentFrag = homeFrag;
+                    transaction.show(homeFrag).commit();
                     break;
                 case R.id.nav_search:
                     toolbarTitle.setText("Search");
-                    selectedFrag = new SearchFragment();
+
+                    //if fragment is null or not loaded then added to the transaction stack otherwise the previous one hides and the tapped on is shown
+                    if (searchFrag == null) {
+                        searchFrag = new SearchFragment();
+                        currentFrag = searchFrag;
+                        transaction.add(R.id.activity_main_frame_layout, searchFrag, "1").commit();
+                        return true;
+                    }
+                    currentFrag = searchFrag;
+                    transaction.show(searchFrag).commit();
                     break;
                 case R.id.nav_notifications:
                     toolbarTitle.setText("Notifications");
-                    selectedFrag = new NotificationsFragment();
+                    if (notifFrag == null) {
+                        notifFrag = new NotificationsFragment();
+                        currentFrag = notifFrag;
+                        transaction.add(R.id.activity_main_frame_layout, notifFrag, "2").commit();
+                        return true;
+                    }
+                    currentFrag = notifFrag;
+                    transaction.show(notifFrag).commit();
                     break;
                 case R.id.nav_profile:
                     toolbarTitle.setText("Profile");
-                    selectedFrag = new ProfileFragment();
+                    if (profileFrag == null) {
+                        profileFrag= new ProfileFragment();
+                        currentFrag = profileFrag;
+                        transaction.add(R.id.activity_main_frame_layout, profileFrag, "3").commit();
+                        return true;
+                    }
+                    currentFrag = profileFrag;
+                    transaction.show(profileFrag).commit();
                     break;
                 default:
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout, selectedFrag).commit();
             return true;
         }
     };
+
+    private void setRootFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        toolbarTitle.setText("Home");
+        homeFrag = new HomeFragment();
+        currentFrag = homeFrag;
+        fragmentManager.beginTransaction().add(R.id.activity_main_frame_layout,homeFrag, "0").commit();
+    }
+
 }
