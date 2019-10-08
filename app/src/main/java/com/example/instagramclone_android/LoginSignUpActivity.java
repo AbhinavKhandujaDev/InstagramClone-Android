23f98de62f98ac96d3991a743abb890f5434ccc4
Abhinav <abhikhanduja@gmail.com>
2019-10-08 21:33:13 +0530

    LoginSignUpActivity fragment switch animations added

--- app/src/main/java/com/example/instagramclone_android/LoginSignUpActivity.java
+++ app/src/main/java/com/example/instagramclone_android/LoginSignUpActivity.java
@@ -1,14 +1,57 @@
 package com.example.instagramclone_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class LoginSignUpActivity extends AppCompatActivity {
+import com.example.instagramclone_android.Fragments.HomeFragment;
+import com.example.instagramclone_android.Fragments.LoginFragment;
+import com.example.instagramclone_android.Fragments.SignUpFragment;
+import com.example.instagramclone_android.Model.Interfaces.LoginSignUpInterface;
+
+public class LoginSignUpActivity extends AppCompatActivity implements LoginSignUpInterface {
+
+    Fragment loginFragment;
+    Fragment signUpFragment;
+
+    FragmentManager fragmentManager;
+
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sign_up);
+
+        frameLayout = findViewById(R.id.activity_login_signup_frame_layout);
+        setRootFragment();
+    }
+
+    private void setRootFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        loginFragment = new LoginFragment(this);
+        fragmentManager.beginTransaction().add(R.id.activity_login_signup_frame_layout,loginFragment, "0").commit();
+    }
+
+    @Override
+    public void signInTapped() {
+        onBackPressed();
+    }
+
+    @Override
+    public void signUpTapped() {
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right);
+        signUpFragment = new SignUpFragment(this);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.add(R.id.activity_login_signup_frame_layout,signUpFragment).commit();
     }
 }
---
+++ app/src/main/java/com/example/instagramclone_android/Model/Interfaces/LoginSignUpInterface.java
@@ -0,0 +1,6 @@
+package com.example.instagramclone_android.Model.Interfaces;
+
+public interface LoginSignUpInterface {
+    void signInTapped();
+    void signUpTapped();
+}
